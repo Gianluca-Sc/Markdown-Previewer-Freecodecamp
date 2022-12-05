342bdf6f13a7f70e21972f55ffbb9c2bd2606af7
Gianluca-Sc <scibettagianluca@gmail.com>
2022-12-05 23:57:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import MarkdownPreview from "@uiw/react-markdown-preview";
+import { useEffect, useState } from "react";
+
+const MARKDOWN_TEXT =
+  "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, '`<div></div>`', between 2 backticks.\n\n'```'\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n'```'\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)";
 
 function App() {
+  const [markdown, setMarkdown] = useState(
+    localStorage.getItem("markdown") || MARKDOWN_TEXT
+  );
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  const switchTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-color-mode", theme);
+  }, [theme]);
+
+  useEffect(() => {
+    localStorage.setItem("markdown", markdown);
+  }, [markdown]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="top-bar">
+        <h1>Markdown Previewer</h1>
+        <div className="toggle" onClick={switchTheme}></div>
+      </div>
+      <div className="container">
+        <div className="editor">
+          <textarea
+            id="editor"
+            name="markdown"
+            cols="80"
+            rows="80"
+            onChange={(e) => setMarkdown(e.target.value)}
+            value={markdown}
+          ></textarea>
+        </div>
+        <div className="previewer">
+          <div className="wmde-markdown-var">
+            <MarkdownPreview
+              id="preview"
+              source={markdown}
+              warpperElement={{ "data-color-mode": theme }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
